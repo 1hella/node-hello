6e7b93d74dfb03f8380334e7a03d734cb26a6d74
Stephen Wanhella <stephen.wanhella@gmail.com>
2015-08-07 19:47:13 -0700

    Formatting

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-var express = require('express'),
-    app = express(),
-    cons = require('consolidate'); // Templating library adapter for Express
+var express = require('express');
+var app = express();
+var cons = require('consolidate'); // Templating library adapter for Express
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
-    res.render('hello', { name : 'World' });
+  res.render('hello', { name : 'World' });
 });
 
 app.get('*', function(req, res){
-  console.log(req);
-    res.render('hello', { name : req.url.substr(1, req.url.length) });
+  res.render('hello', { name : req.url.substr(1, req.url.length) });
 });
 
 app.listen(process.env.PORT || 5000);
-console.log('Express server started on port 8080');
+
